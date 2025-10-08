b5213c320589e33de069d0784690a2daa79971b1
Lazyjimpressions <lazyjimpressions@gmail.com>
2025-10-08 15:40:20 -0500

    Fix Marketing Shell template - show page content instead of React mount point

--- wp-content/themes/amaa-tmr/page-marketing.php
+++ wp-content/themes/amaa-tmr/page-marketing.php
@@ -6,19 +6,13 @@ Template Post Type: page
 get_header(); ?>
 
 <main id="content" class="site">
-    <?php if (is_front_page()) : ?>
-        <!-- React HomePage Component Mount Point -->
-        <div id="homepage-root"></div>
-    <?php else : ?>
-        <!-- Default content for other marketing pages -->
-        <div class="container">
-            <?php
-            while (have_posts()) : the_post();
-                the_content();
-            endwhile;
-            ?>
-        </div>
-    <?php endif; ?>
+    <div class="container">
+        <?php
+        while (have_posts()) : the_post();
+            the_content();
+        endwhile;
+        ?>
+    </div>
 </main>
 
 <?php get_footer();
