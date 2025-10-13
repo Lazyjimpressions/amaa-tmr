453dc6d5dd13e56524de97b8236126540b1580c4
Lazyjimpressions <lazyjimpressions@gmail.com>
2025-10-12 19:48:48 -0500

    Update survey script version to 1.0.6 in JavaScript file

--- wp-content/themes/amaa-tmr/assets/js/survey-island.js
+++ wp-content/themes/amaa-tmr/assets/js/survey-island.js
@@ -2,7 +2,7 @@
 (function() {
     'use strict';
 
-    console.log('NEW MULTI-PAGE SURVEY LOADING - VERSION 1.0.5');
+        console.log('NEW MULTI-PAGE SURVEY LOADING - VERSION 1.0.6');
 
     // Global no-op for showNotification to prevent errors
     window.showNotification = window.showNotification || function() {};
